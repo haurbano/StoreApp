9ae420b5407c7a1c38a7834025beb963373989fe
haurbano <haurbano.dev@gmail.com>
2020-06-26 19:56:40 -0500

    Added koin as DI framework

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,12 +5,15 @@ internal object Versions {
     const val appCompatVersion = "1.1.0"
     const val jUnitVersion = "4.12"
     const val jUnitExtAndroidVersion = "1.1.1"
+    const val koinVersion = "2.1.5"
 }
 
 object Dependencies {
     const val kotlin        = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersion}"
     const val coreKtx       = "androidx.core:core-ktx:${Versions.coreKtxVersion}"
     const val appCompat     = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
+    const val koin          = "org.koin:koin-core:${Versions.koinVersion}"
+    const val koinViewModel = "org.koin:koin-androidx-viewmodel:${Versions.koinVersion}"
 }
 
 object TestDependencies {
