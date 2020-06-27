bba6a2bf5dd98d4d974c699ac3eb6da6044bcd21
haurbano <haurbano.dev@gmail.com>
2020-06-27 00:40:47 -0500

    Added repositoriesModule to koin configuration

--- app/src/main/java/com/haurbano/mymercadolibre/MyMercadolibreApp.kt
+++ app/src/main/java/com/haurbano/mymercadolibre/MyMercadolibreApp.kt
@@ -1,6 +1,7 @@
 package com.haurbano.mymercadolibre
 
 import android.app.Application
+import com.haurbano.data.di.repositoriesModule
 import com.haurbano.domain.di.useCasesModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -10,7 +11,10 @@ class MyMercadolibreApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            modules(useCasesModule)
+            modules(
+                useCasesModule,
+                repositoriesModule
+            )
             androidContext(this@MyMercadolibreApp)
         }
     }
