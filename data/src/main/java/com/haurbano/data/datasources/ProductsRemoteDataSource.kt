ea67288820e00add2bcc5b83d5303bc0c079721d
haurbano <haurbano.dev@gmail.com>
2020-06-27 05:02:32 -0500

    Added implementation to get product details in the ProductsRepositoryImpl.kt and added method to the abstract datasource

--- data/src/main/java/com/haurbano/data/datasources/ProductsRemoteDataSource.kt
+++ data/src/main/java/com/haurbano/data/datasources/ProductsRemoteDataSource.kt
@@ -1,7 +1,9 @@
 package com.haurbano.data.datasources
 
-import com.haurbano.domain.models.Product
+import com.haurbano.domain.models.ProductDetails
+import com.haurbano.domain.models.ProductPreview
 
 interface ProductsRemoteDataSource {
-    suspend fun searchProductsBy(query: String): List<Product>
+    suspend fun searchProductsBy(query: String): List<ProductPreview>
+    suspend fun getProductDetails(productId: String): ProductDetails
 }
--- data/src/main/java/com/haurbano/data/repositories/ProductsRepositoryImpl.kt
+++ data/src/main/java/com/haurbano/data/repositories/ProductsRepositoryImpl.kt
@@ -1,14 +1,19 @@
 package com.haurbano.data.repositories
 
 import com.haurbano.data.datasources.ProductsRemoteDataSource
-import com.haurbano.domain.models.Product
+import com.haurbano.domain.models.ProductDetails
+import com.haurbano.domain.models.ProductPreview
 import com.haurbano.domain.respositories.ProductsRepository
 
 class ProductsRepositoryImpl(
     private val remoteDataSource: ProductsRemoteDataSource
 ) : ProductsRepository {
 
-    override suspend fun searchProductsBy(query: String): List<Product> {
+    override suspend fun searchProductsBy(query: String): List<ProductPreview> {
         return remoteDataSource.searchProductsBy(query)
+    }
+
+    override suspend fun getProductDetails(productId: String): ProductDetails {
+        return remoteDataSource.getProductDetails(productId)
     }
 }
